8be7b82c7306a999eb4e40bbb0f295784284306d
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-14 21:13:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,5 @@
 board = ["nil", " ", "X", " ", " ", " ", " ", " ", " "]
 index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 def position_taken?(board, index)
-  if board[index] == " "
-    print "true"
+  !(board[0] == "nil" || board[1] == " ")
   end
