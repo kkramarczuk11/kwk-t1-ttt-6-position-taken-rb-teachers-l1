305c588d4f1654bcaf0465744b1f80b4760debe2
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-14 21:16:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
 # code your #position_taken? method here!
+board = Array[" ", " ", "X", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, index)
+  board[0] == "X" || !board[index] == " "
+  end
